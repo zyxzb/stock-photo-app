3ac850981ffae59cbda67850fd0c05bf49de38fe
Bartek Z <bartekzarzyk@gmail.com>
2022-10-06 21:57:00 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import {FaSearch} from 'react-icons/fa';
+import Photo from './components/Photo';
 import './App.css';
 
+
+const mainURL = `https://api.unsplash.com/photos/`
+// const searchURL = `https://api.unsplash.com/search/photos/`
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  
+  console.log(pageNumber);
+
+    const fetchImages = async() => {
+      setLoading(true);
+        let url;
+        url = `${mainURL}?client_id=${process.env.REACT_APP_ACCESS_KEY}&page=${pageNumber}`
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            setPhotos(prevData => {
+              return ([...prevData, ...data])
+            });
+            setLoading(false);
+
+        } catch(error) {
+          alert(error);
+          setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+      fetchImages()
+    }, [pageNumber])
+
+    useEffect(() => {
+      const event = window.addEventListener("scroll", () => {
+          // console.log(`inner height: ${window.innerHeight}`);
+          // scrollY returns the pixels a document has scrolled from the upper left corner of the window
+          // console.log(`scrollY: ${window.scrollY}`);
+          // console.log(`body height: ${document.body.scrollHeight}`);
+
+          if(!loading && (window.innerHeight + window.scrollY) >= (document.body.scrollHeight - 10)) {
+              console.log('it works');
+              setPageNumber(prevState => prevState +1 )
+          }
+      })
+      return () => window.removeEventListener('scroll', event);
+    },[])
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+    }
+
+    return (
+        <main className="App">
+          <section className='form-container'>
+                <div>
+                  <h1>Unsplash API</h1>
+                </div>
+                <form>
+                  <input type="text" placeholder='Search...'/>
+                  <button onClick={handleSubmit}><FaSearch/></button>
+                </form>
+          </section>
+          <section className='container'>
+            <div className='imgs-container'>
+              {photos.map((photo, index) => (
+                <Photo key={index} {...photo}/>
+              ))}
+            </div>
+            {loading && <span>Loading...</span>}
+          </section>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/Photo.jsx
@@ -0,0 +1,91 @@
+import React,{useState} from 'react';
+import { AiOutlineLink } from "react-icons/ai";
+import { MdStarOutline, MdStar } from "react-icons/md";
+// MdStarHalfe 1/2 star
+import './photo.css';
+
+
+const Photo = ({urls:{regular}, alt_description, likes, user:{name, portfolio_url, profile_image:{medium}}}) => {
+    const [hover, setHover] = useState(false)
+    const [changeSize, setChangeSize] = useState(false);
+
+    return (
+        <> 
+        <div 
+        className={hover ? 'photo-wrapper active' : 'photo-wrapper'}
+        onClick={() => setChangeSize(!changeSize)}
+        >
+            <img 
+            className={changeSize ? 'image active':'image'}
+            src={regular} 
+            alt={alt_description} 
+            onMouseOver={()=> setHover(true)}
+            onMouseOut={()=> setHover(false)}
+            />
+            <div className='info'>
+                <div className='likes-info'>
+                    <h4>{name}</h4>
+                    <p 
+                    // className={likes < 15 ? "red" : likes <50 ? "orange" : "green"}
+                    >
+                        {/* {likes}  */}
+                        {/* {`likes`} {likes < 15 ? "😪" : likes <50 ? "🤨" : "😍"} */}
+                        {likes < 10 ?(
+                        <>
+                        <MdStar/> 
+                        <MdStarOutline/> 
+                        <MdStarOutline/> 
+                        <MdStarOutline/> 
+                        <MdStarOutline/> 
+                        </> 
+                        )
+                        :likes < 20 ?(
+                            <>
+                            <MdStar/> 
+                            <MdStar/> 
+                            <MdStarOutline/> 
+                            <MdStarOutline/> 
+                            <MdStarOutline/> 
+                            </> 
+                            )
+                        :likes < 30 ?(
+                            <>
+                            <MdStar/> 
+                            <MdStar/> 
+                            <MdStar/>
+                            <MdStarOutline/> 
+                            <MdStarOutline/> 
+                            </> 
+                            )
+                        :likes < 50 ?(
+                            <>
+                            <MdStar/> 
+                            <MdStar/> 
+                            <MdStar/>
+                            <MdStar/>
+                            <MdStarOutline/> 
+                            </> 
+                            )
+                        : (
+                            <>
+                            <MdStar/> 
+                            <MdStar/> 
+                            <MdStar/>
+                            <MdStar/>
+                            <MdStar/>
+                            </> 
+                            )
+                    }
+                        </p>
+                </div>
+                <a href={portfolio_url} target="_blank" rel="noopener noreferrer">
+                    {portfolio_url && <AiOutlineLink size={25}/>}
+                    <img src={medium} alt={name} />
+                </a>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Photo;
